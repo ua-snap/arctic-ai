479d45beef19c6b97bd35737728804c8fe3d54b9
joshdpaul <jdpaul3@alaska.edu>
2024-12-19 13:01:32 -0900

    update functions for txt database and finish nb

--- luts.py
+++ luts.py
@@ -1,7 +1,7 @@
 urls = {
-    "Fairbanks.txt": "https://northernclimatereports.org/report/community/AK124",
-    "Anchorage.txt": "https://northernclimatereports.org/report/community/AK15",
-    "Juneau.txt": "https://northernclimatereports.org/report/community/AK172",
-    "Nome.txt": "https://northernclimatereports.org/report/community/AK280",
-    "Utqiagvik.txt": "https://northernclimatereports.org/report/community/AK418",
+    "data/Fairbanks.txt": "https://northernclimatereports.org/report/community/AK124",
+    "data/Anchorage.txt": "https://northernclimatereports.org/report/community/AK15",
+    "data/Juneau.txt": "https://northernclimatereports.org/report/community/AK172",
+    "data/Nome.txt": "https://northernclimatereports.org/report/community/AK280",
+    "data/Utqiagvik.txt": "https://northernclimatereports.org/report/community/AK418",
 }
--- populate_database.py
+++ populate_database.py
@@ -1,11 +1,13 @@
 import argparse
 import os
 import shutil
-from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 from get_embedding_function import get_embedding_function
 from langchain_community.vectorstores.chroma import Chroma
+
+from luts import urls
 
 import warnings
 
@@ -32,14 +34,14 @@ def main():
 
 
 def load_documents():
-    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    document_loader = DirectoryLoader(DATA_PATH, glob="**/*.txt")
     return document_loader.load()
 
 
 def split_documents(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=80,
+        chunk_size=400,
+        chunk_overlap=40,
         length_function=len,
         is_separator_regex=False,
     )
@@ -77,29 +79,11 @@ def add_to_chroma(chunks: list[Document]):
 
 def calculate_chunk_ids(chunks):
 
-    # This will create IDs like "data/monopoly.pdf:6:2"
-    # Page Source : Page Number : Chunk Index
-
-    last_page_id = None
-    current_chunk_index = 0
+    # This will add URLs to the metadata of each chunk.
+    # These are not unique to each chunk, but that is OK - we just want to provide a reference to the source website.
 
     for chunk in chunks:
-        source = chunk.metadata.get("source")
-        page = chunk.metadata.get("page")
-        current_page_id = f"{source}:{page}"
-
-        # If the page ID is the same as the last one, increment the index.
-        if current_page_id == last_page_id:
-            current_chunk_index += 1
-        else:
-            current_chunk_index = 0
-
-        # Calculate the chunk ID.
-        chunk_id = f"{current_page_id}:{current_chunk_index}"
-        last_page_id = current_page_id
-
-        # Add it to the page meta-data.
-        chunk.metadata["id"] = chunk_id
+        chunk.metadata["url"] = urls[chunk.metadata["source"]]
 
     return chunks
 
