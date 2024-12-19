acf8ac1d5b756797b7f09ab0d60a082b4dab7c17
joshdpaul <jdpaul3@alaska.edu>
2024-12-19 12:42:34 -0900

    NCR text

---
+++ luts.py
@@ -0,0 +1,7 @@
+urls = {
+    "Fairbanks.txt": "https://northernclimatereports.org/report/community/AK124",
+    "Anchorage.txt": "https://northernclimatereports.org/report/community/AK15",
+    "Juneau.txt": "https://northernclimatereports.org/report/community/AK172",
+    "Nome.txt": "https://northernclimatereports.org/report/community/AK280",
+    "Utqiagvik.txt": "https://northernclimatereports.org/report/community/AK418",
+}
