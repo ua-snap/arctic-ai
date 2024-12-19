a01d5df4ce445f4624fb40a564bd7486812f8106
joshdpaul <jdpaul3@alaska.edu>
2024-12-19 13:42:15 -0900

    k=3 for db similarity search

--- query_data.py
+++ query_data.py
@@ -37,7 +37,7 @@ def query_rag(query_text: str):
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_score(query_text, k=5)
+    results = db.similarity_search_with_score(query_text, k=3)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
